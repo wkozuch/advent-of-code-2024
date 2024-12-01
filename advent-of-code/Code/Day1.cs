66331797f1fa03787ee18ec5bf16ecafd2b926d7
Wojciech Kożuch <wkozuch@gmail.com>
2024-12-01 20:45:39 +0100

    Add Advent of Code 2024 - Day1
    Solution for Day1

---
+++ advent-of-code/Code/Day1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+  class Day1
+  {
+    public static void Main(string[] args)
+    {
+      var lines = File.ReadAllLines(@"Datasets\Day1.txt");
+      var col1 = new List<int>();
+      var col2 = new List<int>();
+      foreach (var line in lines)
+      {
+        var cells = line.Split("   ");
+        col1.Add(int.Parse(cells.First()));
+        col2.Add(int.Parse(cells.Last()));
+      }
+      col1 = col1.OrderBy(x => x).ToList();
+      col2 = col2.OrderBy(x => x).ToList();
+      var sum1 = col1.Zip(col2, (a, b) => Math.Abs(a - b)).Sum();
+      var sum2 = col1.Select(n => n * col2.Count(x => x == n)).Sum();
+      Console.WriteLine($"Part one: {sum1}");
+      Console.WriteLine($"Part two: {sum2}");
+    }
+  }
+}
